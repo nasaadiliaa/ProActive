81d513db88d15f70b0db9f92b8e53146b7dd03db
dawwas110104 <dawwas.inha@gmail.com>
2024-12-12 04:46:52 +0700

    remove client

--- client/config-overrider.js
+++
@@ -1,8 +0,0 @@
-const { override, addBabelPlugins } = require('customize-cra');
-
-module.exports = override(
-  ...addBabelPlugins(
-    "@babel/plugin-proposal-optional-chaining",
-    "@babel/plugin-proposal-nullish-coalescing-operator"
-  )
-);
